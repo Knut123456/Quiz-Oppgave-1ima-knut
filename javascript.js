a72146fbc368c6109099198cf25d8bdc13489bb5
Knut123456 <knroa002@osloskolen.no>
2023-11-10 13:08:14 +0100

    gjorde mer justeringer med liv

--- javascript.js
+++ javascript.js
@@ -1,11 +1,23 @@
+
+// const make it so that Id on html get on
 const answerInput = document.getElementById("answerInput");
 const submitAnswerButton = document.getElementById("submitAnswer");
 const resultDisplay = document.getElementById("result");
+const tekst = document.getElementById ("counter")
+
+
+let liv = Number(localStorage.getItem("liv"))
+
 
 const correctAnswer = "markus persson"; 
 
 submitAnswerButton.addEventListener("click", function () {
     const playerAnswer = answerInput.value.trim().toLowerCase(); 
+    liv--;
+    tekst.textContent = liv
+    if (liv == 0) {
+        window.location.href = "feil.html"
+    }
 
     if (playerAnswer == correctAnswer) {
         resultDisplay.textContent = "Riktig svar!";
@@ -14,6 +26,13 @@ submitAnswerButton.addEventListener("click", function () {
         }, 3000);
     } else {
         resultDisplay.textContent = "Feil svar. Prøv igjen.";
+        liv --;
     }
 });
+
+
+
+
+
+
 
---
+++ startscript.js
@@ -0,0 +1,1 @@
+localStorage.setItem("liv", 3)
